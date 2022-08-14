31b0ab1753eb94080fa91ff3c25b36fc4a44ff99
Chad Buie <chadbuie@gmail.com>
2022-08-14 18:32:27 -0400

    created a response object that usings JSON to return data with a condition of true.

--- httpServer.js
+++ httpServer.js
@@ -1,10 +1,41 @@
 const http = require("http");
 
+// Mock Data For API Calls
+
+const data = [
+  {
+    id: 1,
+    firstName: "Chad",
+    lastNane: "Buie",
+    email: "chadbuie@gmail.com",
+  },
+  {
+    id: 2,
+    firstName: "Amanda",
+    lastNane: "Rowe",
+    email: "arowe2004@gmail.com",
+  },
+  {
+    id: 3,
+    firstName: "Elias",
+    lastNane: "Rafa",
+    email: "eliasrafat@gmail.com",
+  },
+];
+
 // this is an example you will use Express JS Lib In The Future
 // this variable server creates an http server
+// const server = http.createServer((req, res) => {
+//  console.log("First Web Server Started.");
+// });
+
 const server = http.createServer((req, res) => {
-  console.log("Web Server Started.");
-});
+  res.end(
+      JSON.stringify({
+          success : true, 
+          data : data
+      })
+  )
 
 server.listen(3000, () => {
   console.log("Server is started");
